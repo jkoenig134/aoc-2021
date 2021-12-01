4e1714f82732703578e91cf85d37b37190b933b9
jkoenig <julian.koenig@js-soft.com>
2021-12-01 12:10:01 +0100

    chore: move template

---
+++ _archive/template.dart
@@ -0,0 +1,23 @@
+import '../lib/input_reader.dart';
+
+main(List<String> args) {
+  var testInput = InputReader.test(1).asInt();
+  print("""Test
+  (1) ${part1(testInput)}
+  (2) ${part2(testInput)}
+  """);
+
+  var input = InputReader(1).asInt();
+  print("""Real
+  (1) ${part1(input)}
+  (2) ${part2(input)}
+  """);
+}
+
+int part1(List<int> input) {
+  return 0;
+}
+
+int part2(List<int> input) {
+  return 0;
+}
