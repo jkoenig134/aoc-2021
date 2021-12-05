3b6e444cdea9c42b32688e07bee43b7080a02e4c
jkoenig <julian.koenig@js-soft.com>
2021-12-05 13:24:22 +0100

    feat: implement day05

--- day05.dart
+++ day05.dart
@@ -1,23 +1,95 @@
+import 'dart:math';
+
 import './lib/input_reader.dart';
 
+class Position {
+  final int x;
+  final int y;
+
+  Position(this.x, this.y);
+
+  @override
+  String toString() {
+    return "${x},${y}";
+  }
+}
+
+class Path {
+  late Position start;
+  late Position end;
+
+  Path.fromString(String input) {
+    final split = input.split(" -> ");
+
+    final startSplit = split[0].split(",");
+    start = Position(int.parse(startSplit[0]), int.parse(startSplit[1]));
+
+    final endSplit = split[1].split(",");
+    end = Position(int.parse(endSplit[0]), int.parse(endSplit[1]));
+  }
+
+  List<Position> calculatePositions(bool includeDiagonals) {
+    if (start.x == end.x) {
+      final startY = min(start.y, end.y);
+      final endY = max(start.y, end.y);
+
+      return List.generate(
+        endY - startY + 1,
+        (i) => Position(start.x, startY + i),
+      );
+    } else if (start.y == end.y) {
+      final startX = min(start.x, end.x);
+      final endX = max(start.x, end.x);
+
+      return List.generate(
+        endX - startX + 1,
+        (i) => Position(startX + i, start.y),
+      );
+    }
+
+    if (!includeDiagonals) return [];
+
+    return [
+      for (var x = start.x, y = start.y;
+          x != end.x;
+          x += (start.x > end.x ? -1 : 1), y += (start.y > end.y ? -1 : 1))
+        Position(x, y),
+      Position(end.x, end.y),
+    ];
+  }
+}
+
+int findCommonPoints(List<String> input, [bool includeDiagonals = false]) {
+  final paths = input.map((line) => Path.fromString(line)).toList();
+  final positions = paths
+      .map((e) => e.calculatePositions(includeDiagonals))
+      .expand((e) => e)
+      .map((e) => e.toString())
+      .toList();
+
+  var occurences = Map<String, int>();
+  positions.forEach(
+    (position) =>
+        occurences[position] = occurences.putIfAbsent(position, () => 0) + 1,
+  );
+
+  return occurences.values.where((v) => v > 1).length;
+}
+
 main(List<String> args) {
-  var testInput = InputReader.test(5).asInt();
+  var testInput = InputReader.test(5).asString();
   print("""Test
   (1) ${part1(testInput)}
   (2) ${part2(testInput)}
   """);
 
-  var input = InputReader(5).asInt();
+  var input = InputReader(5).asString();
   print("""Real
   (1) ${part1(input)}
   (2) ${part2(input)}
   """);
 }
 
-int part1(List<int> input) {
-  return 0;
-}
+int part1(List<String> input) => findCommonPoints(input);
 
-int part2(List<int> input) {
-  return 0;
-}
+int part2(List<String> input) => findCommonPoints(input, true);
