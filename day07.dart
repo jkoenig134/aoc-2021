81300bd0e00363648c1a5cd87ac6cc9005df1b09
jkoenig <julian.koenig@js-soft.com>
2021-12-07 08:36:31 +0100

    feat: implement day07

--- day07.dart
+++ day07.dart
@@ -1,23 +1,36 @@
+import 'dart:math';
+
+import './lib/extensions.dart';
 import './lib/input_reader.dart';
 
 main(List<String> args) {
-  var testInput = InputReader.test(7).asInt();
+  final testInput = InputReader.test(7, ",").asInt();
   print("""Test
   (1) ${part1(testInput)}
   (2) ${part2(testInput)}
   """);
 
-  var input = InputReader(7).asInt();
+  final input = InputReader(7, ",").asInt();
   print("""Real
   (1) ${part1(input)}
   (2) ${part2(input)}
   """);
 }
 
-int part1(List<int> input) {
-  return 0;
+int calculateMinimalCost(List<int> input, int Function(int) costFunction) {
+  Map<int, int> costForSteps = {};
+
+  final results = Set<int>.from(input).map(
+    (n) => input
+        .map((e) => (n - e).abs())
+        .map((e) => costForSteps.putIfAbsent(e, () => costFunction(e)))
+        .sum,
+  );
+
+  return results.reduce(min);
 }
 
-int part2(List<int> input) {
-  return 0;
-}
+int part1(List<int> input) => calculateMinimalCost(input, (e) => e);
+
+int part2(List<int> input) =>
+    calculateMinimalCost(input, (e) => [for (var i = 1; i <= e; i++) i].sum);
--- lib/input_reader.dart
+++ lib/input_reader.dart
@@ -1,19 +1,21 @@
 import 'dart:io';
 
 class InputReader {
+  final String splitPattern;
+
   final String _input;
-  InputReader(int day) : _input = File('./input/$day.txt').readAsStringSync();
-  InputReader.test(int day)
+  InputReader(int day, [this.splitPattern = "\n"])
+      : _input = File('./input/$day.txt').readAsStringSync();
+  InputReader.test(int day, [this.splitPattern = "\n"])
       : _input = File('./input/$day.test.txt').readAsStringSync();
 
   String raw() => _input;
 
-  List<String> asString() => _input.split("\n");
+  List<String> asString() => _input.split(this.splitPattern);
+  List<int> asInt() => asString().map(int.parse).toList();
 
   List<String> asNewlineString() =>
       _input.split("\n\n").map((e) => e.replaceAll("\n", " ")).toList();
-
-  List<int> asInt() => asString().map(int.parse).toList();
 
   List<List<String>> asStringList() =>
       _input.split("\n").map((e) => e.split("")).toList();
