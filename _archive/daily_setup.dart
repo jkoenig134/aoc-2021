6333dc2b8b492ebc77fd2550443b1f58456871de
jkoenig <julian.koenig@js-soft.com>
2021-12-07 13:00:48 +0100

    chore: open editors for daily setup

--- _archive/daily_setup.dart
+++ _archive/daily_setup.dart
@@ -4,6 +4,10 @@ import 'package:http/http.dart' as http;
 
 void main() async {
   final day = DateTime.now().day;
+  final dayAsString = day.toString().padLeft(2, "0");
+  await Process.run("code", ["day$dayAsString.dart"]);
+  await Process.run("code", ["input/$day.test.txt"]);
+  await Process.run("code", ["input/$day.txt"]);
 
   final cookie = File("_archive/cookie.secret").readAsStringSync();
   final response = await http.get(
