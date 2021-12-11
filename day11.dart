af8347b7a137b6dff541f262f4e57a38894a9887
jkoenig <julian.koenig@js-soft.com>
2021-12-11 10:19:27 +0100

    feat: add day11

--- day11.dart
+++ day11.dart
@@ -1,23 +1,102 @@
+import './lib/extensions.dart';
 import './lib/input_reader.dart';
 
+class Octopus {
+  final int x;
+  final int y;
+
+  int lightLevel;
+  bool flashed = false;
+
+  Octopus(this.x, this.y, this.lightLevel);
+
+  void increaseLightlevel() => lightLevel++;
+
+  bool reset() {
+    if (!flashed) return false;
+
+    lightLevel = 0;
+    flashed = false;
+    return true;
+  }
+}
+
+class OctopusGrid {
+  final List<List<Octopus>> octoGrid;
+  late List<Octopus> octoList;
+
+  int _steps = 0;
+
+  OctopusGrid(List<List<int>> input)
+      : octoGrid = [
+          for (var x = 0; x < input.length; x++)
+            [
+              for (var y = 0; y < input[x].length; y++)
+                Octopus(
+                  x,
+                  y,
+                  input[x][y],
+                )
+            ]
+        ] {
+    octoList = octoGrid.expand((x) => x).toList();
+  }
+
+  List<Octopus> surroundingOctos(int x, int y) {
+    final maxLength = octoGrid.length - 1;
+    return [
+      if (x > 0 && y > 0) octoGrid[x - 1][y - 1],
+      if (y > 0) octoGrid[x][y - 1],
+      if (y > 0 && x < maxLength) octoGrid[x + 1][y - 1],
+      if (x > 0) octoGrid[x - 1][y],
+      if (x < maxLength) octoGrid[x + 1][y],
+      if (x > 0 && y < maxLength) octoGrid[x - 1][y + 1],
+      if (y < maxLength) octoGrid[x][y + 1],
+      if (x < maxLength && y < maxLength) octoGrid[x + 1][y + 1]
+    ];
+  }
+
+  void increaseLightLevelOfSourrounding(Octopus octo) {
+    if (octo.flashed || octo.lightLevel < 9) return;
+    octo.flashed = true;
+    surroundingOctos(octo.x, octo.y).forEach((o) => o.increaseLightlevel());
+  }
+
+  int flash() {
+    _steps++;
+    octoList.forEach((e) => e.increaseLightlevel());
+
+    while (octoList.any((e) => e.lightLevel > 9 && !e.flashed)) {
+      octoList
+          .where((e) => e.lightLevel > 9 && !e.flashed)
+          .forEach((e) => increaseLightLevelOfSourrounding(e));
+    }
+
+    return octoGrid
+        .map((e) => e.map((e) => e.reset()).where((f) => f).length)
+        .sum;
+  }
+
+  int synchronize() {
+    while (flash() != 100) {}
+    return _steps;
+  }
+}
+
 main(List<String> args) {
-  final testInput = InputReader.test(11).asInt();
+  final testGrid = OctopusGrid(InputReader.test(11).asIntList());
   print("""Test
-  (1) ${part1(testInput)}
-  (2) ${part2(testInput)}
+  (1) ${part1(testGrid)}
+  (2) ${part2(testGrid)}
   """);
 
-  final input = InputReader(11).asInt();
+  final grid = OctopusGrid(InputReader(11).asIntList());
   print("""Real
-  (1) ${part1(input)}
-  (2) ${part2(input)}
+  (1) ${part1(grid)}
+  (2) ${part2(grid)}
   """);
 }
 
-int part1(List<int> input) {
-  return 0;
-}
+int part1(OctopusGrid grid) => [for (var i = 0; i < 100; i++) grid.flash()].sum;
 
-int part2(List<int> input) {
-  return 0;
-}
+int part2(OctopusGrid grid) => grid.synchronize();
