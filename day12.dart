37b2e5ba6057bd1485bfccb6e91f3e10e22c184e
jkoenig <julian.koenig@js-soft.com>
2021-12-12 09:23:51 +0100

    feat: add day12

--- day12.dart
+++ day12.dart
@@ -1,23 +1,85 @@
 import './lib/input_reader.dart';
 
 main(List<String> args) {
-  final testInput = InputReader.test(12).asInt();
+  final testInput = Graph.fromInput(InputReader.test(12).asString());
   print("""Test
   (1) ${part1(testInput)}
   (2) ${part2(testInput)}
   """);
 
-  final input = InputReader(12).asInt();
+  final input = Graph.fromInput(InputReader(12).asString());
   print("""Real
   (1) ${part1(input)}
   (2) ${part2(input)}
   """);
 }
 
-int part1(List<int> input) {
-  return 0;
+class Point {
+  final String name;
+
+  Point(this.name);
 }
 
-int part2(List<int> input) {
-  return 0;
+class Graph {
+  late int numberOfVertices;
+  final Map<String, List<String>> neighbours = {};
+
+  Graph(this.numberOfVertices);
+
+  Graph.fromInput(List<String> input) {
+    final entries = input.map((e) => e.split("-"));
+
+    final uniquePoints = entries.expand((element) => element).toSet();
+    numberOfVertices = uniquePoints.length;
+
+    entries.forEach((e) => addEdge(e[0], e[1]));
+  }
+
+  void addEdge(String u, String v) {
+    (neighbours[u] ??= []).add(v);
+    (neighbours[v] ??= []).add(u);
+  }
+
+  List<List<String>> getAllPaths(String start, String end, bool p2) =>
+      _calculatePaths(start, end, [start], p2, false);
+
+  List<List<String>> _calculatePaths(
+    String currentNode,
+    String end,
+    List<String> currentPath,
+    bool twoSmallCavesAllowed,
+    bool enteredSmallCaveTwice,
+  ) {
+    if (currentNode == end) {
+      return [List.from(currentPath)];
+    }
+
+    final paths = <List<String>>[];
+
+    for (String n in neighbours[currentNode] ?? []) {
+      final smallDuplicate = currentPath.contains(n) && n.toLowerCase() == n;
+
+      if ((n == "start") ||
+          (smallDuplicate && !twoSmallCavesAllowed) ||
+          (smallDuplicate && enteredSmallCaveTwice)) {
+        continue;
+      }
+
+      currentPath.add(n);
+      paths.addAll(_calculatePaths(
+        n,
+        end,
+        currentPath,
+        twoSmallCavesAllowed,
+        enteredSmallCaveTwice || smallDuplicate,
+      ));
+      currentPath.removeLast();
+    }
+
+    return paths;
+  }
 }
+
+int part1(Graph graph) => graph.getAllPaths("start", "end", false).length;
+
+int part2(Graph graph) => graph.getAllPaths("start", "end", true).length;
