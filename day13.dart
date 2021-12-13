4cf537b397d90e799389eeb54bf39dbf574d7665
jkoenig <julian.koenig@js-soft.com>
2021-12-13 10:16:52 +0100

    feat: add day13

--- day13.dart
+++ day13.dart
@@ -1,23 +1,103 @@
+import 'dart:math';
+
+import './lib/extensions.dart';
 import './lib/input_reader.dart';
 
+class Point {
+  final int x;
+  final int y;
+
+  Point(this.x, this.y);
+}
+
+enum Direction { x, y }
+
+class Instruction {
+  final Direction direction;
+  final int position;
+
+  Instruction.fromString(String instruction)
+      : direction = Direction.values.byName(instruction[11]),
+        position = int.parse(instruction.substring(13));
+}
+
+class TransparentPaper {
+  late List<Instruction> instructions;
+  late List<List<String>> paper;
+
+  TransparentPaper(String input) {
+    final split = input.split("\n\n");
+
+    final points = split[0]
+        .split("\n")
+        .map((e) => e.split(","))
+        .map((e) => Point(int.parse(e[0]), int.parse(e[1])));
+
+    final maxX = points.map((e) => e.x).reduce(max);
+    final maxY = points.map((e) => e.y).reduce(max);
+
+    paper = List.generate(maxY + 1, (y) => List.generate(maxX + 1, (x) => " "));
+    points.forEach((p) => paper[p.y][p.x] = "#");
+
+    instructions =
+        split[1].split("\n").map((e) => Instruction.fromString(e)).toList();
+  }
+
+  TransparentPaper fold() {
+    final instruction = instructions.removeAt(0);
+
+    final newPaper =
+        instruction.direction == Direction.x ? paper : paper.flipped().toList();
+
+    for (var i = 0; i < newPaper.length; i++) {
+      final row = newPaper[i];
+
+      final newRow = row.sublist(0, instruction.position);
+
+      final leftOver = row.sublist(instruction.position + 1);
+      for (var j = 0; j < leftOver.length; j++) {
+        final newRowIndex = newRow.length - 1 - j;
+
+        if (newRow[newRowIndex] == " ") {
+          newRow[newRowIndex] = leftOver[j];
+        }
+      }
+
+      newPaper[i] = newRow;
+    }
+
+    paper = instruction.direction == Direction.x
+        ? newPaper
+        : newPaper.flipped().toList();
+
+    return this;
+  }
+
+  int get taggedFields => paper.map((e) => e.where((e) => e == "#").length).sum;
+
+  String finishFolding() {
+    while (instructions.isNotEmpty) {
+      fold();
+    }
+
+    return paper.map((row) => row.join("")).join("\n");
+  }
+}
+
 main(List<String> args) {
-  final testInput = InputReader.test(13).asInt();
+  final testInput = TransparentPaper(InputReader.test(13).raw());
   print("""Test
   (1) ${part1(testInput)}
-  (2) ${part2(testInput)}
+  (2)\n${part2(testInput)}
   """);
 
-  final input = InputReader(13).asInt();
+  final input = TransparentPaper(InputReader(13).raw());
   print("""Real
   (1) ${part1(input)}
-  (2) ${part2(input)}
+  (2)\n${part2(input)}
   """);
 }
 
-int part1(List<int> input) {
-  return 0;
-}
+int part1(TransparentPaper paper) => paper.fold().taggedFields;
 
-int part2(List<int> input) {
-  return 0;
-}
+String part2(TransparentPaper input) => input.finishFolding();
