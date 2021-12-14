4ad6f5482fb00aef9bfa86a3ce47a74895e1319b
jkoenig <julian.koenig@js-soft.com>
2021-12-14 10:04:38 +0100

    feat: add day14

--- day14.dart
+++ day14.dart
@@ -1,23 +1,82 @@
+import 'dart:math';
+
+import './lib/extensions.dart';
 import './lib/input_reader.dart';
 
+class Polymerization {
+  late Map<String, String> _rules;
+  Map<String, int> _tupleCount = {};
+
+  late String _lastChar;
+
+  Polymerization(String input) {
+    final split = input.split("\n\n");
+
+    _rules = {
+      for (final line in split[1].split("\n"))
+        line.substring(0, 2): line.substring(6)
+    };
+
+    final formula = split[0];
+    formula
+        .split("")
+        .windowed(2)
+        .map((e) => e.join())
+        .forEach((e) => _tupleCount[e] = (_tupleCount[e] ?? 0) + 1);
+    _lastChar = formula[formula.length - 1];
+  }
+
+  Polymerization polymerizeFor(int count) {
+    for (var i = 0; i < count; i++) {
+      polymerize();
+    }
+
+    return this;
+  }
+
+  void polymerize() {
+    final Map<String, int> newCount = {};
+
+    for (String k in _tupleCount.keys) {
+      final insertion = _rules[k]!;
+
+      final firstHalf = k[0] + insertion;
+      final secondHalf = insertion + k[1];
+
+      newCount[firstHalf] = (newCount[firstHalf] ?? 0) + _tupleCount[k]!;
+      newCount[secondHalf] = (newCount[secondHalf] ?? 0) + _tupleCount[k]!;
+    }
+
+    _tupleCount = newCount;
+  }
+
+  int calculate() {
+    final Map<String, int> charCount = {};
+
+    for (String k in _tupleCount.keys) {
+      charCount[k[0]] = charCount.putIfAbsent(k[0], () => 0) + _tupleCount[k]!;
+    }
+
+    charCount[_lastChar] = charCount[_lastChar]! + 1;
+
+    return charCount.values.reduce(max) - charCount.values.reduce(min);
+  }
+}
+
 main(List<String> args) {
-  final testInput = InputReader.test(14).asInt();
+  final testInput = Polymerization(InputReader.test(14).raw());
   print("""Test
   (1) ${part1(testInput)}
   (2) ${part2(testInput)}
   """);
 
-  final input = InputReader(14).asInt();
+  final input = Polymerization(InputReader(14).raw());
   print("""Real
   (1) ${part1(input)}
   (2) ${part2(input)}
   """);
 }
 
-int part1(List<int> input) {
-  return 0;
-}
+int part1(Polymerization input) => input.polymerizeFor(10).calculate();
 
-int part2(List<int> input) {
-  return 0;
-}
+int part2(Polymerization input) => input.polymerizeFor(30).calculate();
